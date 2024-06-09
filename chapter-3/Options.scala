072de3e972828aa314c011c71dcb51cf89510d20
Josh McHugh <joshua.mchugh01@gmail.com>
2024-06-08 20:45:05 -0400

    More Options examples

--- chapter-3/Options.scala
+++ chapter-3/Options.scala
@@ -10,3 +10,16 @@ hello("Mr", "Haoyi", Some("Li"))
 
 println(s"Some(\"Li\").getOrElse(\"<unknown>\"): ${Some("Li").getOrElse("<unknown>")}")
 println(s"None.getOrElse(\"<unknown>\"): ${None.getOrElse("<unknown>")}")
+
+def hello2(name: Option[String]) =
+  for s <- name do
+    println(s"Hello $s")
+
+hello2(None)
+hello2(Some("Haoyi"))
+
+def nameLength(name: Option[String]) =
+  name.map(_.length).getOrElse(-1)
+
+println(s"nameLength(Some(\"Haoyi)): ${nameLength(Some("Haoyi"))}")
+println(s"nameLength(None): ${nameLength(None)}")
