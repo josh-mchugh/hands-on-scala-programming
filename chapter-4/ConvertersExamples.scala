101c760f3a8fb15868597a2deccd43481f20c09b
Josh McHugh <joshua.mchugh01@gmail.com>
2024-06-09 23:46:53 -0400

    Converters Example

---
+++ chapter-4/ConvertersExamples.scala
@@ -0,0 +1,8 @@
+println("Converters Example")
+
+val vector = Array(1, 2, 3).toVector
+println(s"vector: $vector")
+val array = Vector(1, 2, 3).toArray
+println(s"array: $array")
+val set = Array(1, 1, 2, 2, 3, 4).toSet
+println(s"set: $set")
