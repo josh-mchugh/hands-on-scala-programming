ce8f42db6278a6d79f4922bc5a6cf67f4121573b
Josh McHugh <joshua.mchugh01@gmail.com>
2024-06-16 08:53:39 -0400

    Example of shortest path with breadth first search algorithm

--- chapter-6/BreadthFirstSearchExample.scala
+++ chapter-6/BreadthFirstSearchExample.scala
@@ -27,3 +27,23 @@ def search[T](start: T, graph: Map[T, Seq[T]]): Set[T] =
 println(s"search(start = \"c\", graph = graph1): ${search(start = "c", graph = graph1)}")
 println(s"search(start = \"a\", graph = graph2): ${search(start = "a", graph = graph2)}")
 println(s"search(start = \"c\", graph = graph2): ${search(start = "c", graph = graph2)}")
+
+def searchPaths[T](start: T, graph: Map[T, Seq[T]]): Map[T, List[T]] =
+  val seen = collection.mutable.Map(start -> List(start))
+  val queue = collection.mutable.ArrayDeque(start -> List(start))
+  while queue.nonEmpty do
+    val (current, path) = queue.removeHead()
+    for next <- graph(current) if !seen.contains(next) do
+      val newPath = next :: path
+      seen(next) = newPath
+      queue.append((next, newPath))
+
+  seen.toMap
+
+def shortestPath[T](start: T, dest: T, graph: Map[T, Seq[T]]): Seq[T] =
+  val shortestReversedPaths = searchPaths(start, graph)
+  shortestReversedPaths(dest).reverse
+
+println(s"searchPaths(start = \"a\", graph = graph2): ${searchPaths(start = "a", graph = graph2)}")
+println(s"shortestPath(start = \"a\", dest = \"d\", graph = graph2): ${shortestPath(start = "a", dest = "d", graph = graph2)}")
+println(s"shortestPath(start = \"a\", dest = \"c\", graph = graph2): ${shortestPath(start = "a", dest = "c", graph = graph2)}")
