42fac06820604bc2b9aeaa1096d8025fdeb1df95
Josh McHugh <joshua.mchugh01@gmail.com>
2024-06-23 16:21:42 -0400

    Example of minimal application

---
+++ chapter-14/app/src/MinimalApplication.scala
@@ -0,0 +1,12 @@
+package app
+
+object MinimalApplication extends cask.MainRoutes:
+  @cask.get("/")
+  def hello() =
+    "Hello World"
+
+  @cask.post("/do-thing")
+  def doThing(request: cask.Request) =
+    request.text().reverse
+
+  initialize()
