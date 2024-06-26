050938c76e5ecd88557855ccedae0c7ea0e2bdbd
Josh McHugh <joshua.mchugh01@gmail.com>
2024-06-25 23:29:49 -0400

    Examples of different types of queries

--- chapter-15/DatabaseExample.scala
+++ chapter-15/DatabaseExample.scala
@@ -55,5 +55,98 @@ case class CountryLanguage(
 //val result = ctx.run(query[Country])
 //println(s"result: $result")
 
-val result = ctx.run(query[CountryLanguage])
+//val result = ctx.run(query[CountryLanguage])
+//println(s"result: $result")
+
+// Filtering
+//val result = ctx.run(query[City].filter(_.name == "Singapore"))
+//println(s"results: $result")
+
+//val result = ctx.run(query[City].filter(_.id == 3208))
+//println(s"results: $result")
+
+//val result = ctx.run(query[City].filter(_.population > 90_000_00))
+//println(s"results: $result")
+
+// Lifting
+def find(cityId: Int) = ctx.run(query[City].filter(_.id == lift(cityId)).map(_.name))
+
+//val result = find(3208)
+//println(s"result: $result")
+
+//val result = find(3209)
+//println(s"result: $result")
+
+// Mapping
+
+//val result = ctx.run(query[Country].map(c => (c.name, c.continent)))
+//println(s"result: $result")
+
+//val result = ctx.run(query[Country].map(c => (c.name, c.continent, c.population)))
+//println(s"result: $result")
+
+//val result = find(3208)
+//println(s"result: $result")
+
+// Joins
+
+//val result = ctx.run(
+//  query[City]
+//    .join(query[Country])
+//    .on(_.countryCode == _.code)
+//    .filter { case (city, country) => country.continent == "Asia" }
+//    .map { case (city, country) => city.name }
+//)
+//println(s"result: $result")
+
+// Inserts
+//ctx.run(query[City].insertValue(City(10000, "test", "TST", "Test Country", 0)))
+
+//val result = ctx.run(query[City].filter(_.population == 0))
+//println(s"result: $result")
+
+//val cities = List(
+//  City(10001, "testville", "TSV", "Test Country", 0),
+//  City(10002, "testopolis", "TSO", "Test Country", 0),
+//  City(10003, "testberg", "TSB", "Test Country", 0)
+//)
+
+//ctx.run(liftQuery(cities).foreach(e => query[City].insertValue(e)))
+
+//val result = ctx.run(query[City].filter(_.population == 0))
+//println(s"result: $result")
+
+// Updates
+
+//ctx.run(
+//  query[City]
+//    .filter(_.id == 10000)
+//    .updateValue(City(10000, "testham", "TST", "Test Country", 0))
+//)
+
+//val result = ctx.run(query[City].filter(_.id == 10000))
+//println(s"result: $result")
+
+//ctx.run(query[City].filter(_.id == 10000).update(_.name -> "testford"))
+
+//val result = ctx.run(query[City].filter(_.id == 10000))
+//println(s"result: $result")
+
+//ctx.run(
+//  query[City].filter(_.district == "Test Country").update(_.district -> "Test Borough")
+//)
+
+//val result = ctx.run(query[City].filter(_.population == 0))
+//println(s"result: $result")
+
+// Transactions
+
+//ctx.transaction {
+//  ctx.run(
+//    query[City].filter(_.district == "Test Borough").update(_.district -> "Test Country")
+//  )
+//  throw new Exception()
+//}
+
+val result = ctx.run(query[City].filter(_.population == 0))
 println(s"result: $result")
